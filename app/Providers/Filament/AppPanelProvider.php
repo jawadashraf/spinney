4f62c27e037337a6f583f409d786484554ad4a59
Jawad Ashraf <jawadashraf78@gmail.com>
2026-02-15 17:52:03 +0000

    config: comment out the app panel domain configuration.

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -60,7 +60,7 @@ final class AppPanelProvider extends PanelProvider
         $panel
             ->default()
             ->id('app')
-            ->domain('app.'.parse_url((string) config('app.url'))['host'])
+            // ->domain('app.'.parse_url((string) config('app.url'))['host'])
             ->homeUrl(fn (): string => CompanyResource::getUrl('index'))
             ->brandName('Spinneyhill')
             ->plugins([
